389f453b7c434297d8c4433daf0e7e1d4f87ea3d
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2023-11-29 16:02:38 +0000

    create page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom";
+import { Home } from "./pages/home/Home";
+import Recipe from "./pages/recipe/Recipe";
+import { Search } from "./pages/search/Search";
+import Create from "./pages/create/Create";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route>
+            <Create path="/create" />
+          </Route>
+
+          <Route path="/search">
+            <Search />
+          </Route>
+
+          <Route path="/recipe">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
