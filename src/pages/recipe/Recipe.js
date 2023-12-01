1d180e17a6967a3e5d765eab5af9b52503ae70ee
Nikita Klimcuks <nikita.klimchuck@gmail.com>
2023-12-01 15:40:55 +0000

    create recipe form

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,16 +1,25 @@
 import{ useParams } from "react-router-dom"
 import { useFetch } from "../../hooks/useFetch"
+import "./Recipe.css"
 
 const Recipe = () => {
   let { id } =  useParams()
 
- const { data, isPending, error} =  useFetch(`http://localhost:3000/recipes/${id}`)
+ const { data: recipe, isPending, error} =  useFetch(`http://localhost:3000/recipes/${id}`)
 
   return (
     <div className="recipe">
     { error && <p className="error">{error}</p>}
     { isPending && <p className="loading">Loading...</p>} 
-    { data && <h3>{ data.title }</h3> }
+    { recipe && 
+      <>
+      <h2 className="page-title">{ recipe.title }</h2> 
+      <p>takes{recipe.cookingTime}</p>
+        <ul>
+          { recipe.ingredients.map(ing => <li>{ing}</li>)}
+        </ul>
+        <p className="method">{ recipe.method }</p>
+      </>}
      </div>
   )
 }
